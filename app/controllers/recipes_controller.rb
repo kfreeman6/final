e01ff1ea0e43b995826306ae7974821b383a7543
vagrant <vagrant@kellogg-dev-box>
2014-06-03 01:56:36 +0000

    buggy in a new way

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -7,21 +7,22 @@ class RecipesController < ApplicationController
                 "notes" => params["notes"],
                 "star_rating" => params["star_rating"],
                 "category_name" => params["category_name"],
-                "recipe_added" => Date.today)
+                "recipe_added" => Date.today,
+                "user_id" => "Placeholder")
 
-    redirect_to "/users/show"
+    redirect_to "/"
   end
 
   def update
-    the_recipe = Recipe.find_by(params["id"])
-    the_recipe.update("recipe_name" => params["recipe_name"],
+    recipe = Recipe.find_by("id" => params["id"])
+    recipe.update("recipe_name" => params["recipe_name"],
                 "recipe_photo" => params["recipe_photo"],
                 "recipe_URL" => params["recipe_URL"],
                 "notes" => params["notes"],
                 "star_rating" => params["star_rating"],
                 "category_name" => params["category_name"])
 
-    redirect_to "/recipes/#{the_recipe["id"]}"
+    redirect_to "/"
   end
 
   def destroy
