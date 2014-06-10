fc4b49250178a7356608cab981c70f45c8bb537b
vagrant <vagrant@kellogg-dev-box>
2014-06-10 02:29:25 +0000

    almost done 2

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140610014936) do
+ActiveRecord::Schema.define(version: 20140610022740) do
 
   create_table "comments", force: true do |t|
     t.integer "recipe_id"
